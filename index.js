8b28c0ef4180ae9bc448e6b9b13a258b5468bec4
Tanvir Alam <ta89725@gmail.com>
2023-07-25 22:58:30 +0600

    api set up done

--- index.js
+++ index.js
@@ -5,15 +5,39 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 const news_catagory = require('./data/categories.json')
+const allNwes = require("./data/news.json")
 
 app.get('/', (request, responce) => {
     responce.send("server running successful")
 })
 
+app.get("/news-category/:id", (request, responce) => {
+    const id = request.params.id
+    const newses = allNwes.filter((news) => news.category_id == id)
+    if (id == 0) {
+        responce.send(allNwes)
+    } else {
+        responce.send(newses)
+    }
+})
 
-app.get("/news-catagory", (request, responce) => {
+app.get("/news", (request, responce) => {
+    responce.send(allNwes)
+})
+
+//single news
+app.get("/news/:id", (request, responce) => {
+    const id = request.params.id
+    const eachNews = allNwes.find((report) => report._id == id)
+    responce.send(eachNews)
+})
+
+
+app.get("/news-category", (request, responce) => {
     responce.send(news_catagory)
 })
+
+
 
 app.listen(port, (request, responce) => {
     console.log(`the-herald-newsn on port ${port}`)
